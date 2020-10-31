52e4d4cf9bb8afc4371f0575e02a151d6aee96db
Vishal Singh <v.singh.kld@gmail.com>
2020-11-01 00:25:19 +0530

    modified sidebar with new look

--- src/components/fragment/SideBar.jsx
+++ src/components/fragment/SideBar.jsx
@@ -1,37 +1,41 @@
 import React, {useContext} from "react";
+import "../assets/scss/SideBar.scss";
 import HistoryIcon from '@material-ui/icons/History';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import AlbumIcon from '@material-ui/icons/Album';
 import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
-import "../assets/scss/SideBar.scss";
 import SideBarOptions from "./SideBarOptions";
 import {ThemeContext} from "../../api/Theme";
 import HoverButton from "./HoverButton";
+import {Add, PlaylistPlay} from "@material-ui/icons";
+import {Button} from "@material-ui/core";
 
 function SideBar() {
     const useStyle = useContext(ThemeContext);
     return (
-        <aside style={useStyle.component} className={"aside-bar"} >
-            <div className="library-container">
+        <aside style={useStyle.component} className={"aside-bar"}>
+            <div className="aside-bar-container">
                 <SideBarOptions className={"lib-sub"} Icon={HistoryIcon} title={"History"}/>
                 <SideBarOptions className={"lib-sub"} Icon={MusicNoteIcon} title={"Songs"}/>
                 <SideBarOptions className={"lib-sub"} Icon={AlbumIcon} title={"Album"}/>
                 <SideBarOptions className={"lib-sub"} Icon={EmojiPeopleIcon} title={"Artist"}/>
             </div>
-            <div className="playlist-container">
-                <div className={"playlist"}>
-                    <p className={"p1"}>MY PLAYLIST</p>
-                    <div className={"list-cont"}>
-                        <HoverButton variant={"text"} text={"All"}/>
-                        <HoverButton variant={"text"} text={"English"}/>
-                        <HoverButton variant={"text"} text={"Hindi"}/>
-                        <HoverButton variant={"text"} text={"Other"}/>
-                    </div>
-                </div>
+            <div className="aside-bar-container playlist">
+                <p className={"p1"}>
+                    <span>MY PLAYLIST</span>
+                    {/*<Button>
+                        <Add/>
+                    </Button>*/}
+                </p>
+                <SideBarOptions className={"lib-sub"} Icon={PlaylistPlay} title={"All"}/>
+                <SideBarOptions className={"lib-sub"} Icon={PlaylistPlay} title={"English"}/>
+                <SideBarOptions className={"lib-sub"} Icon={PlaylistPlay} title={"Hindi"}/>
+                <SideBarOptions className={"lib-sub"} Icon={PlaylistPlay} title={"Other"}/>
             </div>
         </aside>
     );
 }
+
 /*
 *
 * */
