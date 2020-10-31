04982ad2b9263f1b7444470a7126bd3c9c1f9ecc
Vishal Singh <v.singh.kld@gmail.com>
2020-11-01 00:26:35 +0530

    updated search bar functionality and look

--- src/components/Pages/Search.jsx
+++ src/components/Pages/Search.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './css/Search.scss';
 import Container from "../fragment/Container";
+import {useSelector} from "react-redux";
+import MusicCard from "../fragment/MusicCard";
 
 const Search = () => {
+    const {playlists, search} = useSelector(state => state.musicReducer);
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setSearchResult(playlists.filter((i) => (
+            (i.name.toLowerCase().startsWith(search))
+            ||
+            (i.author_name.toLowerCase().startsWith(search))
+            ||
+            (i.musicName.toLowerCase().startsWith(search))
+            ||
+            (i.lang && i.lang.toLowerCase().startsWith(search))
+        )));
+    }, [search, playlists]);
     return (
         <Container>
-            <div className={"Search"}>
-                <h3>Type in search box to search an item</h3>
+            {
+                search === ""
+                &&
+                <div className={"Search"}>
+                    <h3>Type in search box to search an item</h3>
+                </div>
+            }
+            <div className={"Search-result"}>
+                {
+                    searchResult.map((item) => (
+                        <MusicCard key={item.id} music={item}/>
+                    ))
+                }
             </div>
         </Container>
     );
