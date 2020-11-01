0e289121bbd34eeb66ea490c8149123677d9b836
Vishal Singh <v.singh.kld@gmail.com>
2020-11-01 13:28:48 +0530

    update

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Brand from "../fragment/Brand";
+
+const About = () => {
+    return (
+        <div className={"About"}>
+            Icons made by
+            <a href="http://www.freepik.com/" title="Freepik">Freepik</a>
+            from
+            <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+        </div>
+    );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+    import React from "react";
 import ReactDom from 'react-dom';
 import App from './app/App';
 import './index.scss';
